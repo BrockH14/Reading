fe6f078ddcac092d9e2864385c4ed0ea71c89e7d
Brock Horton <brock.horton@gmail.com>
2020-06-20 13:24:44 -0400

    blah

---
+++ client/build/precache-manifest.3b2ee302f4100dcd754b361e60ebe75b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b1b6e20c4e33d92d0549",
+    "url": "/static/css/main.ec8dadc3.chunk.css"
+  },
+  {
+    "revision": "b1b6e20c4e33d92d0549",
+    "url": "/static/js/main.f903fea2.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "219f29db5f10382fa35f",
+    "url": "/static/js/2.600514d9.chunk.js"
+  },
+  {
+    "revision": "019617674c2fcba5a2957b43c5da1ceb",
+    "url": "/index.html"
+  }
+];
